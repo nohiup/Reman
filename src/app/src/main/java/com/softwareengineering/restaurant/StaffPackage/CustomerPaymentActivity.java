ede9e3a117f7f9788b48503dd15089f620e1a80c
21120076 <107020295+hfkhdslgsfs@users.noreply.github.com>
2024-01-17 12:27:19 +0700

    Staff Payment View

---
+++ src/app/src/main/java/com/softwareengineering/restaurant/StaffPackage/CustomerPaymentActivity.java
@@ -0,0 +1,18 @@
+package com.softwareengineering.restaurant.StaffPackage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.softwareengineering.restaurant.R;
+
+
+
+public class CustomerPaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_payment);
+
+    }
+}
